7393aa5c9069de5199961a1d25f43f3bc1790680
JNComputingLIFE <jon2ndrecaj5@gmail.com>
2025-06-16 09:35:19 +0200

    comments

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use Illuminate\Validation\Rule;
 
+//loginForm
 Route::get('/', function () {
     return view('loginUser');
 });
 
 Route::get('/home', function () {
-    return view('Home'); // This matches your Home.blade.php
+    return view('Home'); // matches  Home.blade.php
 });
 
 Route::get('/register', function () {
